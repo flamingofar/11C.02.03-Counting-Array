79da595b67bf12798be51c5a79417aa9ab9e8b35
malte <malt0533@stud.kea.dk>
2022-02-08 12:21:09 +0100

    Counter working

--- js/index.js
+++ js/index.js
@@ -2,3 +2,28 @@
 
 "use strict;";
 console.log("Counting array");
+
+let count = 0;
+let arr = [];
+
+function initFunc() {
+	setTimeout(loopFunc, 1000);
+}
+
+function loopFunc() {
+	if (count < 9) {
+		arr.unshift(count);
+		console.log(arr);
+		count++;
+	} else {
+		arr.length = 9;
+		arr.unshift(count);
+		arr.length = 9;
+		console.log(arr);
+		count++;
+	}
+
+	initFunc();
+}
+
+initFunc();
